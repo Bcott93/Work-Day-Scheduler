ceb714332be1036046a2d5e1bfca542ca19cfa0f
Benjamin <bcott93@gmail.com>
2023-01-30 19:35:33 +0000

    function to set time, timeblock variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,30 @@
+// Initiates Jquery
 $(document).ready
 
-let time = $('#timer')
-// time.moment().format(Do MMMM YYYY hh:mm:ss a)
+// Sets a variable for the time using moment() and formats
+let currentTime = moment().format("Do MMMM YYYY hh:mm:ss a")
+console.log(currentTime)
+
+// Sets a variable to change the attributes of the timeblocks
+let currentTimeColor = function() {
+    $().attr("backgroundcolor", "red")
+}
+let pastTimeColor = function() {
+    $().attr("backgroundcolor", "grey")
+}
+let futureTimeColor = function() {
+    $().attr("backgroundcolor", "green")
+}
+// function to check what time what it is and apply the necessary styling
+function setTime() {
+    for (let i = 0; i < scheduledHours.length; i++) {
+        if (currentTime === [i])
+        timeBlock === currentTimeColor
+    } 
+        if (currentTime <= [i]) {
+        timeBlock === pastTimeColor
+    } else {
+        timeBlock === futureTimeColor
+    }
+}
+    
