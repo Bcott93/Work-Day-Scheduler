e1409c3c1895c7d247e363ccc21551ffee54139e
Benjamin <bcott93@gmail.com>
2023-01-30 19:49:41 +0000

    amended time variables and scheduled hours

--- assets/script.js
+++ assets/script.js
@@ -2,29 +2,29 @@
 $(document).ready
 
 // Sets a variable for the time using moment() and formats
-let currentTime = moment().format("Do MMMM YYYY hh:mm:ss a")
-console.log(currentTime)
+let currentDate = moment().format("Do MMMM YYYY")
+let currentHour = moment().format("HH")
+let currentTime = moment().format("hh:mm:ss a")
+let timeBlockStyle = ""
 
-// Sets a variable to change the attributes of the timeblocks
-let currentTimeColor = function() {
-    $().attr("backgroundcolor", "red")
-}
-let pastTimeColor = function() {
-    $().attr("backgroundcolor", "grey")
-}
-let futureTimeColor = function() {
-    $().attr("backgroundcolor", "green")
-}
+// sets the working hours, enable all hours incase the schedule gets changed in the future
+let scheduledHours = [0, 1, 2, 3, 4, 5, 6, 7, 8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+
 // function to check what time what it is and apply the necessary styling
 function setTime() {
     for (let i = 0; i < scheduledHours.length; i++) {
-        if (currentTime === [i])
-        timeBlock === currentTimeColor
+        if (currentHour === [i])
+            var currentTimeColor = $().attr("backgroundcolor", "red")
+            timeBlockStyle === currentTimeColor
     } 
-        if (currentTime <= [i]) {
-        timeBlock === pastTimeColor
+        if (currentHour <= [i]) {
+            var pastTimeColor = $().attr("backgroundcolor", "grey")
+            timeBlockStyle === pastTimeColor
     } else {
-        timeBlock === futureTimeColor
+            var futureTimeColor = $().attr("backgroundcolor", "green")
+            timeBlockStyle === futureTimeColor
     }
+    console.log(setTime)
 }
     
+setTime
