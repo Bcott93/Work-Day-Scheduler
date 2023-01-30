e359a1f43494333756fefa39a7ffabdb29c59bc0
Benjamin <bcott93@gmail.com>
2023-01-30 22:55:13 +0000

    commented and added Readme

--- assets/script.js
+++ assets/script.js
@@ -11,15 +11,19 @@ let currentTime = parseInt(moment().format("h a"))
 // An array of the working hours
 let scheduledHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-let testTime = parseInt(moment("2023-01-30 08:01:00").format("h a"))
-
+// Loop through the scheduled hours to create the timeBlocks
 for (let i = 0; i < scheduledHours.length; i++) {
+    //Current hour is the parameter which creates the timeblocks and sets the format to (example: 5pm instead of 17)
     let currentHour = moment(scheduledHours[i], "HH").format("h a")
+    // Creates a div, with a column width of 1/12th and addes the currentHour value as the text. 
     let hourDiv = $("<div>").addClass("col-1 hour").text(currentHour)
+    // Creates a textarea with the column width of 5/6ths and allows the user to input data.
     let description = $("<textarea>").addClass("col-10 description").attr("contentEditable", "true")
+    // Creates a button, with the column width of 1/12th, and adds the text "Save"
     let saveBtn = $("<button>").addClass("col-1 saveBtn").text("Save")
+    // Creates a div with the 3 elements above, and puts them into a row.
     let timeBlock = $("<div>").addClass("row").append(hourDiv, description, saveBtn)  
-    
+    // Checks each looped value against the current time, and then styles the row
     if(scheduledHours[i] < currentTime) {
         description.addClass("future")
     }
@@ -30,14 +34,14 @@ for (let i = 0; i < scheduledHours.length; i++) {
         description.addClass("present")
     }
 
-     
+     // Adds the timeBlock to the HTML.
     $(".container").append(timeBlock)
    
 }
 
-$(".saveBtn").on("click", function (event) {
-    let calenderInput = event.target.
-}
+// // $(".saveBtn").on("click", function (event) {
+// //     let calenderInput = event.target.
+// }
 
 
 
